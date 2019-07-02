9b03c5d21c7559b7d4cc758959d2bc677da951e4
WangXiaoXin <wxx1_2_3@163.com>
2019-07-02 10:41:45 +0800

    finish All Manage

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-// var baseurl="http://192.168.43.249:9090/dddd"
-var baseurl="http://192.168.1.105:9090/dddd"
+var baseurl="http://192.168.43.249:9090/dddd"
+// var baseurl="http://192.168.1.105:9090/dddd"
