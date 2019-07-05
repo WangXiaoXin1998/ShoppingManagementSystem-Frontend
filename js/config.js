d98f1bf8e9d2ffe944720b004eb4f673f1387c04
WangXiaoXin <wxx1_2_3@163.com>
2019-07-05 16:24:38 +0800

    update information UI

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-var baseurl="http://192.168.43.249:9090/dddd"
-// var baseurl="http://192.168.1.110:9090/dddd"
+// var baseurl="http://192.168.43.249:9090/dddd"
+var baseurl="http://192.168.1.110:9090/dddd"
